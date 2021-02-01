b9c26e0e25be21f8ff0c101d5fc79b5928d2f270
rickywychoi <cchaff0105@gmail.com>
2021-01-31 20:55:47 -0800

    Removed mongodb connection string from config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  MONGODB_CONNECTION_STRING:
-    'mongodb+srv://rickywychoi:E1XywexnuKNP0jEV@cluster0.oivdb.mongodb.net/merng?retryWrites=true&w=majority',
   SECRET_KEY: 'private secret key'
 }
